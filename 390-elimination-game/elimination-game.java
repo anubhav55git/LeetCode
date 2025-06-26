d092ecc54abf24ad4ef98a232a33569309256cea
Anubhav Prajapati <118794590+anubhav55git@users.noreply.github.com>
2025-06-26 19:11:41 +0530

    Time: 2 ms (58.03%) | Memory: 41.3 MB (97.98%) - LeetSync

---
+++ 390-elimination-game/elimination-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lastRemaining(int n) {
+        int head = 1;
+        int step = 1;
+        boolean leftToRight = true;
+        
+        while (n > 1) {
+            
+            if (leftToRight || n % 2 == 1) {
+                head += step;
+            }
+            step *= 2;
+            n /= 2;
+            leftToRight = !leftToRight; 
+        }
+        
+        return head;
+    }
+}
